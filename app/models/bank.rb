939f4a8ecf23911f1b02ba2733c06e47b4ccba29
Juan Felipe <juanfrg2001@gmail.com>
2023-12-29 12:53:18 -0500

    Adds models an migrations

---
+++ app/models/bank.rb
@@ -0,0 +1,4 @@
+class Bank < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 50 }
+    
+end
--- spec/models/bank_spec.rb
+++ spec/models/bank_spec.rb
@@ -6,25 +6,25 @@ RSpec.describe Bank, type: :model do
       it "is invalid without a name" do
         bank = Bank.new
         expect(bank).to_not be_valid
-        expect(bank.errors[:nombre]).to include("can't be blank")
+        expect(bank.errors[:name]).to include("can't be blank")
       end
 
       it "is valid with a name" do
-        bank = Bank.new(nombre: "Example Bank")
+        bank = Bank.new(name: "Example Bank")
         expect(bank).to be_valid
       end
     end
 
     context "Length" do
       it "is valid with a name with 50 characters or less" do
-        bank = Bank.new(nombre: "A" * 50)
+        bank = Bank.new(name: "A" * 50)
         expect(bank).to be_valid
       end
 
       it "is invalid with a name longer than 50 characters" do
-        bank = Bank.new(nombre: "A" * 51)
+        bank = Bank.new(name: "A" * 51)
         expect(bank).to_not be_valid
-        expect(bank.errors[:nombre]).to include("is too long (maximum is 50 characters)")
+        expect(bank.errors[:name]).to include("is too long (maximum is 50 characters)")
       end
     end
   end
